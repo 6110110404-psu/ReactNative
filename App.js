1403606695a0834434985f34f09a323fc8ba2198
wattana thongsonkleeb <wpsc2642@gmail.com>
2020-08-17 19:29:16 +0700

    Uncompletepart5

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Weather from './components/Weather';
-import Constants from 'expo-constants'
+//import { StyleSheet, View } from 'react-native';
+//import Weather from './components/Weather';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ZipCodeScreen from './screen/ZipCodeScreen';
+import WeatherScreen from './screen/WeatherScreen';
+
+const stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Weather zipCode="90110"/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={ZipCodeScreen}/>
+          <Stack.Screen name="Weather" component={WeatherScreen}/>          
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: Constants.statusBarHeight
-    // flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
+ 
